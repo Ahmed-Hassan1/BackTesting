e2811f173bfeb1eaa81483673315b4d5e1bcb70a
Ahmed Hassan <ahmed.abdelaziz.hasan92@gmail.com>
2024-08-03 21:51:36 +0300

    Finalized ProfitPal

--- BackTest/bt/views.py
+++ BackTest/bt/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-from pathlib import Path
-import os
-import numpy as np
 import pandas as pd
 import pandas_ta
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from backtesting import Backtest, Strategy
-from backtesting.lib import resample_apply,crossover
 
 logging.basicConfig(filename='backtesting.log',level=logging.INFO)
 
@@ -35,6 +31,8 @@ class ProfitPal(Strategy):
     OB=80
     OS=20
 
+    lastTrade=0
+
 
     def init(self):
         self.rsi=self.I(pandas_ta.rsi,pd.Series(self.data.Close),self.rsiLen)
@@ -44,36 +42,59 @@ class ProfitPal(Strategy):
         self.atr=self.I(pandas_ta.atr,pd.Series(self.data.High),pd.Series(self.data.Low),pd.Series(self.data.Close),self.atrLen)
 
     def next(self):
-        upperBand=self.bollinger[2][-1]
-        lowerBand=self.bollinger[0][-1]
-        middleBand=self.bollinger[1][-1]
+        upperBand=self.bollinger[2][-2]
+        lowerBand=self.bollinger[0][-2]
+        middleBand=self.bollinger[1][-2]
         bollingerBandWidth=((upperBand - lowerBand) / middleBand) * 100 * 100
         
 
         current=datetime.strptime(str(self.data.index[-1]),'%Y-%m-%d  %H:%M:%S')
 
-        #520  590
         currentTimeInMinutes=current.hour*60+current.minute
         
-        #logging.info(str(self.data.index[-1])+"  "+str(currentTimeInMinutes>=520 and currentTimeInMinutes<590))
-        if currentTimeInMinutes>=self.StartTime and currentTimeInMinutes<self.StopTime:
-            if bollingerBandWidth >= self.minBB and bollingerBandWidth<=self.maxBB and self.adx[-1]>=self.minADX and self.adx[-1]<=self.maxADX and self.atr[-1]>=self.minATR and self.atr[-1]<=self.maxATR:
-            
-                if (not self.SMAFilter or self.data.Close[-1]>self.sma[-1]) and self.data.Close[-1]<lowerBand and (not self.RSIFilter or self.rsi[-1]<self.OS):
-                    
+        #logging.info(str(self.data.index[-1])+"  "+str(currentTimeInMinutes>=520 and currentTimeInMinutes<590) +"    "+str(currentTimeInMinutes))
+        if currentTimeInMinutes>=self.StartTime and currentTimeInMinutes<self.StopTime and not self.position:
+            if bollingerBandWidth >= self.minBB and bollingerBandWidth<=self.maxBB and self.adx[0][-1]>=self.minADX and self.adx[0][-1]<=self.maxADX and self.atr[-1]>=self.minATR and self.atr[-1]<=self.maxATR:
+                if (not self.SMAFilter or self.data.Close[-1]>self.sma[-1]) and self.data.Close[-1]<self.bollinger[0][-1] and self.data.Close[-2]<self.bollinger[0][-2] and (not self.RSIFilter or self.rsi[-1]<self.OS):
                     self.buy(tp=self.data.Close[-1]+self.TP*0.25,sl=self.data.Close[-1]-self.SL*0.25)
-                elif (not self.SMAFilter or self.data.Close<self.sma[-1]) and self.data.Close[-1]>upperBand and (not self.RSIFilter or self.rsi[-1]>self.OB):
+                elif (not self.SMAFilter or self.data.Close[-1]<self.sma[-1]) and self.data.Close[-1]>self.bollinger[2][-1] and self.data.Close[-2]>self.bollinger[2][-2] and (not self.RSIFilter or self.rsi[-1]>self.OB):
                     self.sell(tp=self.data.Close[-1]-self.TP*0.25,sl=self.data.Close[-1]+self.SL*0.25)
 
 
 
 def profitPal(request):
+    inputs={
+        "StartDate":"2024/03/07",
+        "EndDate":"2024/06/17",
+        "StartTime":"08:40:00",
+        "EndTime":"09:50:00",
+        "TP":"17",
+        "SL":"23",
+        "smaFilter":"Yes",
+        "SMALength":"1650",
+        "bLength":"21",
+        "bDev":"2.4",
+        "adxLen":"14",
+        "minADX":"0",
+        "maxADX":"100",
+        "atrLen":"14",
+        "minATR":"0",
+        "maxATR":"100",
+        "bMin":"1",
+        "bMax":"100",
+        "rsiLen":"14",
+        "OB":"80",
+        "OS":"20",
+        "TF":"1min"
+    }
     if request.method=='GET' and 'rsiLen' in request.GET:
+        startDate=request.GET['StartDate']
+        endDate=request.GET['EndDate']
         start=request.GET['StartTime']
         end=request.GET['EndTime']
         start=datetime.strptime(start,'%H:%M:%S')
         end=datetime.strptime(end,'%H:%M:%S')
-        start=start.hour*60+start.minute
+        start=start.hour*60+start.minute-2
         end=end.hour*60+end.minute
 
         tp=int(request.GET['TP'])
@@ -94,25 +115,29 @@ def profitPal(request):
         rsiL=int(request.GET['rsiLen'])
         ob=float(request.GET['OB'])
         os=float(request.GET['OS'])
+        tf=request.GET['TF']
         
         
         
         
     
-        data = pd.read_csv("static/media/ES_1min_sample.csv")
-        data['timestamp']=pd.to_datetime(data['timestamp'])
-        data = data.set_index('timestamp') 
-        #df=data.resample('5min').agg({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last','Volume':'sum'})
-        #df=df.dropna()
+        # data = pd.read_csv("static/media/ES_1min_sample.csv")
+        # data['timestamp']=pd.to_datetime(data['timestamp'])
+        data = pd.read_csv("static/media/ES.csv")
+        data['timestamp']=pd.to_datetime(data['timestamp'])-timedelta(hours=4,minutes=1)
+        data = data.set_index('timestamp')
+        df=data.resample(tf).agg({'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last','Volume':'sum'})
+        df=df.loc[(startDate+' 00:00:00'):(endDate+' 23:00:00')]
+        df=df.dropna()
 
-        bt = Backtest(data,ProfitPal,cash=10_000)
+        bt = Backtest(df,ProfitPal,cash=10_000)
 
         stats=bt.run(
             StartTime=start,StopTime=end,TP=tp,SL=sl,SMAFilter=smaFil,smaLen=smaL,
             bLen=bL,bDev=bD,adxLen=adxL,minADX=adxmin,maxADX=adxmax,atrLen =atrL,minATR=atrmin,maxATR=atrmax,
             minBB=minb,maxBB=maxb,RSIFilter=rsiFil,rsiLen=rsiL,OB=ob,OS=os)
 
-        bt.plot(filename="profitpal",open_browser=False)
+        #bt.plot(filename="profitpal",open_browser=False,resample=tf)
         removed_stats=stats.drop("_equity_curve")
         removed_stats=removed_stats.drop("_trades")
 
@@ -124,9 +149,9 @@ def profitPal(request):
             else:
                 trades_df['Size'][idx]="Short"
 
-        return render(request,"bt/profitpal.html",{'trades':trades_df.to_html(),"stats":removed_stats.to_frame().to_html()})
+        return render(request,"bt/profitpal.html",{'trades':trades_df.to_html(),"stats":removed_stats.to_frame().to_html(),"form":request.GET})
 
-    return render(request,"bt/profitpal.html")
+    return render(request,"bt/profitpal.html",{"form":inputs})
 
 
 
